b9fbb1ca79aa9b051790a10a4f42fd20a5364c17
Matthew Brand <mbrand@lenned.com>
2018-09-10 20:03:48 -0400

    Extra files

--- IoT/app.js
+++ IoT/app.js
@@ -21,7 +21,7 @@ const deviceToken =
   "142eaa79779314e506c35c471eb49318e3ba643aae49cc665c7fcb36121f5f09";
 
 const notification = new apn.Notification({
-  alert: "Hello, world!",
+  alert: "PATIENT NEEDS HELP",
   sound: "chime.caf",
   mutableContent: 1,
   topic: "com.matthewbrand.IoT",
@@ -31,7 +31,6 @@ const notification = new apn.Notification({
 });
 
 dash.on("detected", () => {
-  console.log("omg found");
   apnProvider.send(notification, deviceToken).then(result => {
     // see documentation for an explanation of result
     console.log(result);
